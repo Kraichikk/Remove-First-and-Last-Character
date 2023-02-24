564ef3d8f3852e8dccf5cc455965ea19888b01f8
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 11:51:41 +0200

    Add project files.

---
+++ Remove First and Last Character/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Remove_First_and_Last_Character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Remove_char("pisiun malenkiy"));
+        }
+        public static string Remove_char(string s)
+        {
+            string val = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i != 0 && i != s.Length - 1)
+                {
+                    val += s[i];
+                }
+            }
+            return val;
+        }
+    }
+}
